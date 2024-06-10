a1f810814180ec22eeb44e7643455dfa4bc88cf5
nlok5923 <nlokhande5923@gmail.com>
2024-06-10 22:43:10 +0530

    wrote mental poker test

--- aze-enc/src/lib.rs
+++ aze-enc/src/lib.rs
@@ -43,13 +43,29 @@ pub fn final_unmask(pub_agg: Point, cipher: CardCipher, r: Scalar) -> Card {
     Card(m)
 }
 
+fn gen_card() -> Point {
+    let g = Point::GENERATOR;
+    let rndm: Scalar = Scalar::from_val([1,1,1,1,1]);
+    let card = g * rndm;
+    card 
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let public_key = keygen([1, 2, 3, 4, 5]);
-        println!("Public key: {:?}", public_key);
+        let secret_key: [u64; 5] = [1, 2, 3, 4, 5];
+        let public_key = keygen(secret_key);
+        let card = gen_card();
+        println!("Plaintext card: {:?}", card);
+        let masking_factor_1: Scalar = Scalar::from_val([2u64, 2u64, 2u64, 2u64 ,2u64]);
+        let masking_factor_2: Scalar = Scalar::from_val([3u64, 3u64, 3u64, 3u64 ,3u64]);
+        let mask_card = mask(public_key, card, masking_factor_1);
+        let remask_card = remask(public_key, mask_card, masking_factor_2);
+        let inter_unmask_card = inter_unmask(public_key, remask_card, masking_factor_2);
+        let final_unmask_card = final_unmask(public_key, inter_unmask_card, masking_factor_1);
+        println!("Card: {:?}", card);
     }
 }
